d530557976932aba7243afd811f6e312a305a293
sivanxpeer <sivan.x.peer@gmail.com>
2021-11-23 20:01:29 +0200

    countABC

---
+++ ex7.js
@@ -0,0 +1,43 @@
+const { performance } = require('perf_hooks');
+
+//function gets a sentence and returns how many times each letter appears
+
+//make an array 
+function countABC(sentence) {
+    sentence = sentence.replace(/\s/g, "");
+    let letterCounter = {};
+    for (letter of sentence) {
+        if (!letterCounter[letter]) {
+            letterCounter[letter] = 1;
+        }
+        else letterCounter[letter] += 1;
+    }
+    return letterCounter;
+}
+
+// check with if for " "
+function countABC1(sentence) {
+    let letterCounter = {};
+    for (letter of sentence) {
+        if (letter !== " ") {
+            if (!letterCounter[letter]) {
+                letterCounter[letter] = 1;
+            }
+            else letterCounter[letter] += 1;
+        }
+    }
+    return letterCounter;
+}
+
+
+//use string methods
+var startTime = performance.now();
+console.log(countABC1("hello my name is sivan"));
+var endTime = performance.now();
+console.log(`Call took ${endTime - startTime} milliseconds`)
+
+var startTime = performance.now();
+console.log(countABC("hello my name is sivan"));
+var endTime = performance.now();
+console.log(`Call took ${endTime - startTime} milliseconds`)
+
