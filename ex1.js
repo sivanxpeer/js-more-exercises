5fa23ba880b4436221311efa5294721757c76efa
sivanxpeer <sivan.x.peer@gmail.com>
2021-11-23 17:31:33 +0200

    printToN

---
+++ ex1.js
@@ -0,0 +1,21 @@
+
+//simple loop
+function printToN(n) {
+    for (let i = 0; i <= n; i++)
+        console.log(i);
+}
+
+//arrow function
+const printToN1 = (n) => {
+    for (let i = 0; i <= n; i++)
+        console.log(i);
+}
+
+
+
+printToN(10);
+console.log("---------------")
+printToN1(5);
+console.log("---------------")
+
+
